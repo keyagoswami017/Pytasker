4ff58df69e66576f9db39dd281de38948be40ba6
keyagoswami017 <keyagoswami017@gmail.com>
2025-06-11 11:50:43 -0400

    Update-Delete-Complete

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from dotenv import load_dotenv
+from bson import ObjectId
+from bson.errors import InvalidId
 import os
 
 
@@ -46,6 +48,54 @@ def get_tasks():
             "status": task["status"]
         })
     return jsonify(result), 200
+
+@app.route('/tasks/<task_id>', methods=['GET'])
+def get_task(task_id):
+    try:
+        task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+        if task:
+            return jsonify({
+                "id": str(task["_id"]),
+                "title": task["title"],
+                "description": task["description"],
+                "status": task["status"]
+            }), 200
+        else:
+            return jsonify({"error": "Task not found"}), 404
+    except InvalidId:
+        return jsonify({"error": "Invalid task ID"}), 400
+
+
+@app.route('/tasks/<task_id>', methods=['PUT'])
+def update_task(task_id):
+    data = request.get_json()
+    updated_task = {}
+
+    if 'title' in data:
+        updated_task['title'] = data['title']
+    if 'description' in data:
+        updated_task['description'] = data['description']
+    if 'status' in data:
+        updated_task['status'] = data['status']
+    
+    result = mongo.db.tasks.update_one(
+        {"_id": ObjectId(task_id)},
+        {"$set": updated_task}
+    )
+
+    if result.matched_count == 1:
+        return jsonify({"msg": "Task updated successfully"}), 200
+    else:
+        return jsonify({"error": "No Changes or no Task not found"}), 404
+    
+@app.route('/tasks/<task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    result = mongo.db.tasks.delete_one({"_id": ObjectId(task_id)})
+
+    if result.deleted_count == 1:
+        return jsonify({"msg": "Task deleted successfully"}), 200
+    else:
+        return jsonify({"error": "Task not found"}), 404    
 
 if __name__ == "__main__":
     app.run(debug=True)
