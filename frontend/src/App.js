8a432ebcf50f390dcc89d65b1a57a0185e9cc0fe
keyagoswami017 <keyagoswami017@gmail.com>
2025-06-16 11:49:59 -0400

    FrontEnd-Setup-React

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {  
+  return (
+    <div>
+      <h1>PyTasker Frontend</h1>
+      <p>Welcome to the React App!</p>
+    </div>
+  )
+}
+export default App;
