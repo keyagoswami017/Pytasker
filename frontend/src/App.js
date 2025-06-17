1e1fba87313150ac40faf77761c79c551eef32f7
keyagoswami017 <keyagoswami017@gmail.com>
2025-06-17 10:29:06 -0400

    FrontEnd-Get-Post-Task

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import TaskList from './components/TaskList'; 
 
 function App() {  
   return (
-    <div>
-      <h1>PyTasker Frontend</h1>
+    <div className='App'>
+      <h1>PyTasker Task Management</h1>
       <p>Welcome to the React App!</p>
+      <TaskList />
     </div>
   )
 }
